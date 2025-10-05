4b89a15f4f03c8cff4a8b90ed12ddd8138c35526
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-10-05 23:39:30 +0500

    Доделана виселица

--- Практики/02.10/6_gallows.py
+++ Практики/02.10/6_gallows.py
@@ -1,41 +1,102 @@
 import os
 
+'''
+Виселица - игра для запуска в терминале, не в PyCharm
+|----+
+|    |
+|    0
+|  /-*-\
+|    |
+|   | |
+|   | |
+|
+|____________
+'''
+
+
 def draw_gallow(stage: int):
-    result = ('|----+\n'
-              '|    |\n')
+    '''
+    Рисует виселицу на экране
 
-    result += '|    '
-    draw_element('0', stage, 1)
-    result += '\n'
+    :param stage: стадия виселицы
+    '''
+    draw('|----+\n|    |')
 
-    result += '|  '
-    if stage <= 1:
-        result += '/-\n'
-    elif stage == 2:
-        result += '  *\n'
-    elif stage == 3:
-        result += '/-*\n'
-    elif stage == 4:
-        result += '/-*-\\\n'
+    draw('\n|    ')
+    check_to_draw('0', stage, 1)
 
-    if stage >= 2:
-        result += '|    |\n'
+    draw('\n|  ')
+    check_to_draw('/-', stage, 3)
+    check_to_draw('*', stage, 2)
+    check_to_draw('-\\', stage, 4)
 
-    print(result)
+    draw('\n|    ')
+    check_to_draw('|', stage, 2)
 
-def draw_element(char: str, stage: int, check: int):
+    draw('\n|   ')
+    check_to_draw('|', stage, 5)
+    draw(' ')
+    check_to_draw('|', stage, 6)
+
+    draw('\n|   ')
+    check_to_draw('|', stage, 5)
+    draw(' ')
+    check_to_draw('|', stage, 6)
+
+    draw('\n|\n|____________')
+
+
+def check_to_draw(char: str, stage: int, check: int):
+    '''
+    Рисует элемент при условии stage >= check,
+    иначе - заполняет элемент пустотой
+
+    :param char: элемент для рисования
+    :param stage: стадия виселицы
+    :param check: необходимая для прорисовки элемента стадия виселицы
+    '''
     if stage >= check:
-        return char
+        print(char, end='')
+    else:
+        print(' '*len(char), end='')
 
-    '''
-    |----+
-    |    |
-    |    0
-    |  /-*-\
-    |    |
-    |   | |
-    |   | |
-    |____________
-    '''
 
-draw_gallow(4)
+def draw(char: str):
+    '''
+    Рисует текст без перехода на новую строку
+
+    :param char: элемент для рисования
+    '''
+    print(char, end='')
+
+
+word = 'программирование'
+guess = ''.join(["_" for char in word])
+stage = 0
+attempts = []
+
+while (stage <= 5) and (word != guess):
+    os.system('cls')
+    print('\n\nВИСЕЛИЦА\n')
+    draw_gallow(stage)
+    print(f"\n\nВы уже предлагали следующие буквы: {attempts}")
+    print(f"\nОтгадываемое слово: {guess}")
+
+    input_char = input('\nВведите букву: ')
+
+    if len(input_char) != 1 or input_char in attempts:
+        continue
+    if (input_char in word):
+        guess = ''.join([guess[i] if char != input_char else input_char for i, char in enumerate(word)])
+    else:
+        stage += 1
+    attempts += input_char
+
+os.system('cls')
+if word == guess:
+    print("ВЫ ВЫИГРАЛИ!!!\n")
+    print(f"Загаданным словом было: {word}\n\n^_^")
+else:
+    print("ВЫ ПРОИГРАЛИ\n")
+    print(f"Загаданным словом было: {word}\n\n>_<")
+input()
