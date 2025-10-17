b831f55d401d1f4ec8be1968abdd435d85870d7d
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-10-17 23:00:57 +0500

    Петы/расписание: переоформлена структура проекта

--- Петы/schedule.py
+++ Петы/schedule.py
@@ -1,40 +1,49 @@
 from typing import List
 
-def create_schedule(pool: dict, *args: dict):
-    for key, value in pool.items():
-        #if key == 'набор':
-        #    continue
-        for i, item in enumerate(value):
-            if item is None:
-                continue
-            for arg in args:
-                if arg[key][i] is None and not(item[0] in arg['набор']):
-                    if item[1] != 0:
-                        item[1] -= 1
-                        arg[key][i] = item[0]
-                        arg['набор'].append(item[0])
-    return args
+
+class StudentIOT:
+    def __init__(self):
+        self.schedule = {
+            "день1": [None, None, None],
+            "день2": [None, None, None],
+            "день3": [None, None, None],
+            "набор": [],
+        }
+        self.subjects = [
+            "адии-практика",
+            "диск-лекция",
+            "матан-лекция",
+            "матан-практика",
+        ]
 
 
-pool = {
-    'день1': [['адии-практика', 1], ['диск-лекция', 1], ['матан-практика', 1]],
-    'день2': [['адии-практика', 1], ['диск-лекция', 1], None],
-    'день3': [['матан-лекция', 2], ['матан-практика', 1], None],
-    #'набор': ['адии-практика', 'диск-лекция', 'матан-лекция', 'матан-практика']
-}
+class IOT:
+    def __init__(self, n: int):
+        self.pool = {
+            "день1": [["адии-практика", 1], ["диск-лекция", 1], ["матан-практика", 1]],
+            "день2": [["адии-практика", 1], ["диск-лекция", 1], None],
+            "день3": [["матан-лекция", 2], ["матан-практика", 1], None],
+        }
+        self.students = [StudentIOT()] * n
 
-group_1 = {
-    'день1': [None, None, None],
-    'день2': [None, None, None],
-    'день3': [None, None, None],
-    'набор': []
-}
+        for key, value in self.pool.items():
+            print(
+                f'{key}: {", ".join(
+                [f"{subject[0]} (ост: {subject[1]})" if subject is not None else "-------" for subject in value]
+            )}'
+            )
 
-group_2 = {
-    'день1': [None, None, None],
-    'день2': [None, None, None],
-    'день3': [None, None, None],
-    'набор': []
-}
+    def form_schedule(self):
+        for key, value in self.pool.items():
+            for i, item in enumerate(value):
+                if item is None:
+                    continue
+                for group in self.students:
+                    if group[key][i] is None and not (item[0] in group["набор"]):
+                        if item[1] != 0:
+                            item[1] -= 1
+                            group[key][i] = item[0]
+                            group["набор"].append(item[0])
 
-print(create_schedule(pool, group_1, group_2)[1])
+
+my_iot = IOT(3)
