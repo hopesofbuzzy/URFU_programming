e72ed054cf530fd2ca9cdae8b380966b49d3d273
hopesofbuzzy <nurbaevdaniar0@gmail.com>
2025-11-05 19:12:06 +0500

    Доделана третья практика 30.10

--- Практики/30.10/Практика_3/main.py
+++ Практики/30.10/Практика_3/main.py
@@ -1,5 +1,4 @@
 import requests
-from random import randint
 
 
 class Pokemon:
@@ -13,34 +12,61 @@ class Pokemon:
         информация о покемоне
         """
         # Ищем нужного покемона
-        search_response = requests.get(
-            f"https://pokeapi.co/api/v2/pokemon/{index}"
-        )
+        search_response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{index}")
         search_results = search_response.json()
         # Присваиваем значения
         self.name = search_results["name"]
-        self.types = [
-            typed["type"]["name"] for typed in search_results["types"]
-        ]
+        self.types = [typed["type"]["name"] for typed in search_results["types"]]
         self.weight = search_results["weight"]
         self.height = search_results["height"]
         self.abilities = [
-            abilityd["ability"]["name"]
-            for abilityd in search_results["abilities"]
+            abilityd["ability"]["name"] for abilityd in search_results["abilities"]
         ]
-        self.moves = [
-            moved["move"]["name"] for moved in search_results["moves"]
-        ]
+        self.moves = [moved["move"]["name"] for moved in search_results["moves"]]
+        for stat in search_results["stats"]:
+            stat_name = stat["stat"]["name"]
+            if stat_name == "hp":
+                self.hp = int(stat["base_stat"])
+            elif stat_name == "attack":
+                self.attack = int(stat["base_stat"])
+            elif stat_name == "defense":
+                self.defense = int(stat["base_stat"])
+        # Значения для биты
+        self.fight_hp = self.hp
 
     def __str__(self):
         """Вывсти покемона в строчном формате"""
         return (
             f"Имя: {self.name}, "
-            f'Тип: {", ".join(self.types)}, '
+            f"Тип: {', '.join(self.types)}, "
             f"Вес: {self.weight}, "
             f"Рост: {self.height}, "
-            f'Способности: {", ".join(self.abilities)}.'
+            f"Способности: {', '.join(self.abilities)} "
+            f"Здоровье: {self.hp} "
+            f"Атака: {self.attack} "
+            f"Защита: {self.defense}."
         )
+
+    def attack_enemy(self, enemy):
+        """
+        Атаковать покемона с силой self.attack
+
+        :param enemy: покемон для атаки
+        """
+        return enemy.get_damage(self.attack)
+
+    def get_damage(self, attack):
+        """
+        Получить урон силой max(attack//2, attack - self.defense)
+
+        :param attack: сила атаки
+        """
+        self.fight_hp -= max(attack // 2, attack - self.defense)
+        return max(attack // 2, attack - self.defense)
+
+    def rest(self):
+        """Дать Покемону отдохнуть, чтобы он восполнил силы"""
+        self.fight_hp = self.hp
 
 
 class Team:
@@ -92,28 +118,38 @@ class Team:
         """Вывести подробную информацию о команде"""
         print("\n".join([str(pokemon) for pokemon in self.pokemons]))
 
-    def train(self, name1: str, name2: str):
-        """
-        Провести тренировочный бой двух покемонов
 
-        :param name1: имя первого покемона
-        :param name2: имя второго покемона
-        """
-        pokemon1 = self.get_pokemon(name1)
-        pokemon2 = self.get_pokemon(name2)
-        if pokemon1 is not None and pokemon2 is not None:
-            print(f"Бой между {name1} и {name2}!")
-            if randint(1, 2) == 1:
-                print(f"Победил {name1}")
-            else:
-                print(f"Победил {name2}")
+def train(pokemon1: Pokemon, pokemon2: Pokemon):
+    """
+    Провести тренировочный бой двух покемонов
+
+    :param pokemon1: первый покемон
+    :param pokemon2: второй покемон
+    """
+    if pokemon1 is not None and pokemon2 is not None:
+        print("======================")
+        print(f"Начинаем бой между {pokemon1.name} и {pokemon2.name}!")
+        pokemons = [pokemon1, pokemon2]
+        while all([pokemon.fight_hp > 0 for pokemon in pokemons]):
+            print(
+                f"Текущий баланс сил: {pokemon1.name} {pokemon1.fight_hp}/{pokemon2.fight_hp} {pokemon2.name}"
+            )
+            print(f"{pokemons[0].name} наносит удар силы {pokemons[0].attack}")
+            print(f"{pokemons[1].name} теряет {pokemons[0].attack_enemy(pokemons[1])}")
+            print("======================")
+            pokemons.reverse()
         else:
-            print("Бой между покемонами не в команде невозможен!")
+            print(f"Победил {max(pokemons, key=lambda x: x.fight_hp).name}")
+    else:
+        print("Бой должен быть между покемончиками!")
 
 
 my_team = Team(Pokemon("golduck"), Pokemon("caterpie"), Pokemon("charmeleon"))
-my_team.print_info()
 my_team.add(Pokemon("pikachu"))
-my_team.train("pikachu", "charmeleon")
+
+my_pikachu = my_team.get_pokemon("pikachu")
+my_charmeleon = my_team.get_pokemon("charmeleon")
+train(my_charmeleon, my_pikachu)
+
 my_team.remove("charmeleon")
 my_team.print_info()
